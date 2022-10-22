bb40fd56c4fdc8253560e7e320616f09b38fd2d4
vikas <manojkotary@gmail.com>
2022-10-22 22:03:04 +0530

    atlas config done

--- my-blog/backend/server.js
+++ my-blog/backend/server.js
@@ -1,1 +1,25 @@
-// this is server
+const express = require('express');
+const bodyParser = require('body-parser');
+const { MongoClient } = require('mongodb');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const withDB = async (operations, res) => {
+    try {
+        const client = await MongoClient.connect('mongodb+srv://VIKAS:VIKAS@firstcluster.1fyyzpk.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true });
+        const db = client.db('my-blog');
+    
+        // await operations(db);
+
+        console.log('Connected to database');
+    
+        client.close();
+    } catch (error) {
+        // res.status(500).json({ message: 'Error connecting to db', error });
+        console.log(error);
+    }
+    }
+
+    withDB();
