956fc4410d1c903bfaeaf1d2407b30029f642124
vikas <manojkotary@gmail.com>
2022-10-22 21:05:43 +0530

    frontend done

--- my-blog/frontend/src/Components/Articles.js
+++ my-blog/frontend/src/Components/Articles.js
@@ -8,11 +8,11 @@ const Articles = ({articles}) => {
             <div className="p-4 md:w-1/2">
                 <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
                     <Link to={`/article/${article.name}`}>
-                        <img className="lg:h-48 md:h-36 w-full object-cover object-center" src={articles.image} alt="blog" />
+                        <img className="lg:h-48 md:h-36 w-full object-cover object-center" src={article.thumbnail} alt="blog" />
                     </Link>
                     <div className="p-6">
                         <Link to={`/article/${article.name}`}>
-                            <h3 className="title-font text-lg font-medium text-gray-900 mb-3">{articles.title}</h3>
+                            <h3 className="title-font text-lg font-medium text-gray-900 mb-3">{article.title}</h3>
                         </Link>
 
                         <p className="leading-relaxed mb-3">{article.content[0].substring(0, 150)}...</p>
